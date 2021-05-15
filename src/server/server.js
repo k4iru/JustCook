550433c479096ae9a6d5eb021ade227dca66428b
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-15 11:34:31 -0400

    styling

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,6 @@ app.use(express.json());
 // serve files in dist folder (this is the client)
 app.use(express.static('dist'));
 
-
 // client makes api call to server routes
 app.get('/api/test', (req, res) => {
     res.json({test: 'test'});
