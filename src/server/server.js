47b6c2481319a6a1f72b4bf3829799d6375fdd65
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-18 22:01:03 -0400

    search bar

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,26 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
+// create a .env file with API_KEY=YOUR-API-KEY
+const api_key = process.env.API_KEY;
 
 // serve files in dist folder (this is the client)
 app.use(express.static('dist'));
 
+// api call to search spoonacular for recipes
+app.get('/api/search', (req, res) => {
+    const base_url = 'https://api.spoonacular.com/recipes/complexSearch';
+    res.send('test');
+});
 // client makes api call to server routes
 app.get('/api/test', (req, res) => {
     res.json({test: 'test'});
-})
+});
 
 app.listen(8080, () => {
     console.log('listening on 8080');
-})
+});
