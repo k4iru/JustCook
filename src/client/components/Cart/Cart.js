b2247d8548388a90e586024a8e6caaa1c0dd4807
stevendatle <stevendatle@yahoo.com>
2021-05-19 13:05:25 -0400

    click event when viewing a recipe

--- src/client/components/Cart/Cart.js
+++ src/client/components/Cart/Cart.js
@@ -4,3 +4,55 @@ import styles from "./Cart.css";
 import { connect } from "react-redux";
 
 import CartItem from "./CartItem/CartItem";
+
+const Cart = ({ cart }) => {
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalItems, setTotalItems] = useState(0);
+  
+    useEffect(() => {
+      //declaring items and price variables to default to 0 - next loop will be getting each item in the cart 
+      let items = 0;
+      let price = 0;
+  
+      //getting each item in the cart and then multiplying the price with the quantity
+      cart.forEach((item) => {
+        items += item.qty;
+        price += item.qty * item.price;
+      });
+  
+      //setting the totals for item + price
+      setTotalItems(items);
+      setTotalPrice(price);
+    }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
+  
+    //after logic above is done - return the information to the user
+    return (
+      <div className={styles.cart}>
+        <div className={styles.cart__items}>
+          {cart.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </div>
+        <div className={styles.cart__summary}>
+          <h4 className={styles.summary__title}>Cart Summary</h4>
+          <div className={styles.summary__price}>
+            <span>TOTAL: ({totalItems} items)</span>
+            <span>$ {totalPrice}</span>
+          </div>
+          <button className={styles.summary__checkoutBtn}>
+            Proceed To Checkout
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+
+
+const mapStateToProps = state=> {
+    return {
+        cart: state.shop.cart
+    }
+}
+
+export default connect(mapStateToProps)(Cart);
