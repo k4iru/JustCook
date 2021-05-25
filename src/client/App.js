65ac9feb415f8b168441a87c809ffe2d8f1ba646
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-24 21:45:41 -0400

    refactored routing in App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from './components/Home/Home';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 import { connect } from "react-redux";
 
-import Recipes from './components/Recipes/Recipes';
-
+import Recipes from "./components/Recipes/Recipes";
 
 function App() {
   return (
-
     <Router>
       <div>
         <Header />
@@ -19,24 +17,17 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/register">
-            <Home />
-          </Route>
-          <Route path="/recipes">
-            <Recipes />
-          </Route>
-          <Route path="/login">
-            <Home />
-          </Route>
-          <Route path="/favourites">
-            <Recipes />
-          </Route>
-          <Route path="/cart">
-            <Home />
-          </Route>
-          <Route path="/" >
-            <Home />
-          </Route>
+          <Route exact path="/register" component={Home} />
+
+          <Route exact path="/recipes" component={Recipes} />
+
+          <Route exact path="/login" component={Home} />
+
+          <Route exact path="/favourites" component={Recipes} />
+
+          <Route exact path="/cart" component={Home} />
+
+          <Route exact path="/" component={Home} />
         </Switch>
         <Footer />
       </div>
