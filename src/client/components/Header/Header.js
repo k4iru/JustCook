43640fb8c3f247d4db8e793dfdb76260d9b8b4a4
Kyle Cheung <cheung.kyle1@gmail.com>
2021-05-24 20:08:04 -0400

    fixed font awesome

--- src/client/components/Header/Header.js
+++ src/client/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { HeaderItems } from "./HeaderItems";
 import './Header.css';
+import {FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 
  class Header extends React.Component {
 
@@ -15,8 +17,13 @@ import './Header.css';
          return(
                 <header>
                     <nav className="HeaderItems">
+                        <FontAwesomeIcon icon={faTimes} />
                         <Link to="/" className="header-logo-link"><h1 className="header-logo">JustCook</h1></Link>
                         <div className="menu-icon" onClick={this.handleClick}>
+
+                            {/* font awesome example. individual icon import */}
+                            <FontAwesomeIcon icon={this.state.clicked ? faTimes : faBars} />
+
                             <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                         </div>
                         <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
