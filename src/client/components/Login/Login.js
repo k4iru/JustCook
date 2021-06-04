ce97de01e2810f420d873397c2a19fdc44243f99
Kyle Cheung <cheung.kyle1@gmail.com>
2021-06-04 00:43:12 -0400

    Update Login.js
    changed syntax to arrow function

--- src/client/components/Login/Login.js
+++ src/client/components/Login/Login.js
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Login.css";
 
-function Login() {
+const Login = () => {
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="page">
       <div className="form-container">
-        <form className="login-form">
+        <form className="login-form" onSubmit={onSubmit}>
           <h2>Login</h2>
           <input className="form-input" type="text" name="email" placeholder="Email" />
           <input className="form-input" type="password" name="password" placeholder="Password"/>
-          <button type="submit">Login</button>
+          <input className="btn" type="submit" value="Login"/>
         </form>
       </div>
     </div>
